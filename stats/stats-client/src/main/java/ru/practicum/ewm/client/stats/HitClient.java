cd1eb5f6c15e863590a105a3a28cab0384023199
Andreyspb82 <amitkin82@yandex.ru>
2023-10-08 17:42:27 +0300

    feat: Выполнить 1 этап диплома. v03

--- stats/stats-client/src/main/java/ru/practicum/ewm/client/stats/HitClient.java
+++ stats/stats-client/src/main/java/ru/practicum/ewm/client/stats/HitClient.java
@@ -3,11 +3,9 @@ package ru.practicum.ewm.client.stats;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.web.client.RestTemplateBuilder;
-import org.springframework.http.ResponseEntity;
 import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
 import org.springframework.stereotype.Service;
 import org.springframework.web.util.DefaultUriBuilderFactory;
-import ru.practicum.ewm.dto.stats.EndpointHitDto;
 
 @Service
 public class HitClient extends BaseClient {
@@ -22,9 +20,5 @@ public class HitClient extends BaseClient {
                         .requestFactory(HttpComponentsClientHttpRequestFactory::new)
                         .build()
         );
-    }
-
-    public ResponseEntity<Object> createEndpointHit(EndpointHitDto endpointHitDto) {
-        return post("", endpointHitDto);
     }
 }
--- stats/stats-client/src/main/java/ru/practicum/ewm/client/stats/StatsClient.java
+++ stats/stats-client/src/main/java/ru/practicum/ewm/client/stats/StatsClient.java
@@ -3,13 +3,9 @@ package ru.practicum.ewm.client.stats;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.web.client.RestTemplateBuilder;
-import org.springframework.http.ResponseEntity;
 import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
 import org.springframework.stereotype.Service;
 import org.springframework.web.util.DefaultUriBuilderFactory;
-
-import java.util.List;
-import java.util.Map;
 
 @Service
 public class StatsClient extends BaseClient {
@@ -24,16 +20,5 @@ public class StatsClient extends BaseClient {
                         .requestFactory(HttpComponentsClientHttpRequestFactory::new)
                         .build()
         );
-    }
-
-    public ResponseEntity<Object> getViewStats(String start, String end, List<String> uris, Boolean unique) {
-        Map<String, Object> parameters = Map.of(
-                "start", start,
-                "end", end,
-                "uris", uris,
-                "unique", unique
-        );
-
-        return get("?start={start}&end={end}&uris={uris}&unique={unique}", parameters);
     }
 }
