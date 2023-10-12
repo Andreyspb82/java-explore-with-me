ce73978aba2cec0b1c5b8672a1893960fefb2ca1
Andreyspb82 <amitkin82@yandex.ru>
2023-10-12 13:13:25 +0300

    style: Выполнить 1 этап диплома. Исправить замечания после 1 проверки. v00

--- stats/stats-server/src/main/java/ru/practicum/ewm/stats/EwmStatsServiceApp.java
+++ stats/stats-server/src/main/java/ru/practicum/ewm/stats/EwmStatsServiceApp.java
@@ -4,8 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class EWMStatsServiceApp {
+public class EwmStatsServiceApp {
     public static void main(String[] args) {
-        SpringApplication.run(EWMStatsServiceApp.class, args);
+        SpringApplication.run(EwmStatsServiceApp.class, args);
     }
 }
